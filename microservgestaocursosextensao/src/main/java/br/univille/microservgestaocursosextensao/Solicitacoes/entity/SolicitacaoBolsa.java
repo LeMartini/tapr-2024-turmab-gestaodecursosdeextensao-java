1d1908ad9e3897bc900f30e2f2bf69d6e3954835
Leticia Martini <111762149+LeMartini@users.noreply.github.com>
2024-11-03 18:35:15 +0000

    WIP: atualizacao solicitacao bolsa

--- microservgestaocursosextensao/src/main/java/br/univille/microservgestaocursosextensao/Solicitacoes/entity/SolicitacaoBolsa.java
+++ microservgestaocursosextensao/src/main/java/br/univille/microservgestaocursosextensao/Solicitacoes/entity/SolicitacaoBolsa.java
@@ -1,5 +1,42 @@
 package br.univille.microservgestaocursosextensao.Solicitacoes.entity;
 
+import java.time.LocalDate;
+
 public class SolicitacaoBolsa {
-    
+    private String idSolicitacao;
+    private String idProjeto;
+    private LocalDate dataSolicitacao;
+    private String statusSolicitacao;
+
+    public String getIdSolicitacao() {
+        return idSolicitacao;
+    }
+
+    public void setIdSolicitacao(String idSolicitacao) {
+        this.idSolicitacao = idSolicitacao;
+    }
+
+    public String getIdProjeto() {
+        return idProjeto;
+    }
+
+    public void setIdProjeto(String idProjeto) {
+        this.idProjeto = idProjeto;
+    }
+
+    public LocalDate getDataSolicitacao() {
+        return dataSolicitacao;
+    }
+
+    public void setDataSolicitacao(LocalDate dataSolicitacao) {
+        this.dataSolicitacao = dataSolicitacao;
+    }
+
+    public String getStatusSolicitacao() {
+        return statusSolicitacao;
+    }
+
+    public void setStatusSolicitacao(String statusSolicitacao) {
+        this.statusSolicitacao = statusSolicitacao;
+    }
 }
